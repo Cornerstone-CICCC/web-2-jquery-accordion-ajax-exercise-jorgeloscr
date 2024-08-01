68079849a6ca631764e92e0313a15552e1599d47
Jorge Ortega <jorgeloscr@hotmail.com>
2024-07-31 17:05:05 -0700

    Exercise completed

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 $(function() {
   // your code here
+
+  $('.accordion-header').on('click', function() {
+    let $content = $(this).next('.accordion-content')
+
+    if ($content.is(':visible')) {
+        $content.slideUp().removeClass('open')
+    } else {
+        $('.accordion-content').slideUp().removeClass('open')
+
+        $content.slideDown().addClass('open')
+    }
+})
+
+$('.todos button').on('click', function() {
+  $.ajax({
+      url: 'https://dummyjson.com/todos',
+      method: 'GET',
+      success: function(response) {
+          const todos = response.todos;
+          const $ul = $('.todos ul');
+          
+          $ul.empty();
+          
+          todos.forEach(function(todo) {
+              $ul.append(`<li>${todo.todo}</li>`);
+          });
+      },
+      error: function() {
+          alert('Failed to load to-do items.');
+      }
+  });
+});
 })
